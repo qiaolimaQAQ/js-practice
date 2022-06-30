2566d8f11e161c1e44506adc834520a1fabc8026
沙口 <jzsjzs2@gmail.com>
2022-06-30 19:34:15 -0400

    debug

--- jspractice/index.js
+++ jspractice/index.js
@@ -1,28 +1,31 @@
 var that;
-class tab{
+class Tab{
     constructor(id){
         that= this;
         this.main = document.querySelector(id);
         this.lists = this.main.querySelectorAll('li');
         this.sections = this.main.querySelectorAll('section');
         this.add = this.main.querySelector('.tabadd');
+        this.ul = this.main.querySelector('.firstnav ul:first-child');
         this.init();
     }
-    init (){
+    init() {
+
         this.add.onclick = this.addTab;
-        for(var i=0;i<this.lists.length;i++){
+        for (var i = 0; i < this.lists.length; i++) {
             this.lists[i].index = i;
             this.lists[i].onclick = this.toggleTab;
-        }
-    } 
 
-    toggleTab (){
-        that.clearclass();
-        this.className = 'liactive';
-        that.sections[this.index].className = 'conactive';  
+        }
     }
 
-    clearclass (){
+    toggleTab (){
+        that.clearClass();
+        this.className = 'liactive';
+        that.sections[this.index].className = 'conactive';
+    }
+
+    clearClass (){
         for(var i = 0; i < this.lists.length;i++){
             this.lists[i].className = '';
             this.sections[i].className = '';
@@ -30,11 +33,15 @@ class tab{
     }
 
     addTab (){
-        
+        that.clearClass();
+        var li = '<li class="liactive"><span>新选项卡</span><span class="iconfont icon-guanbi"></span></li>'
+        that.ul.insertAdjacentHTML('beforeend', li);
+        that.init();
+        console.log(1111);
     }
 
     removeTab (){}
 
     editTab (){}
 }
-new tab('#tab');
+new Tab('#tab');
