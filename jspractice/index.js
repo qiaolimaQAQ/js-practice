b0128675a1d847d0d623f3767060beb1615694fc
沙口 <jzsjzs2@gmail.com>
2022-07-02 21:35:01 -0400

    Tabadd and edit

--- jspractice/index.js
+++ jspractice/index.js
@@ -3,20 +3,36 @@ class Tab{
     constructor(id){
         that= this;
         this.main = document.querySelector(id);
-        this.lists = this.main.querySelectorAll('li');
-        this.sections = this.main.querySelectorAll('section');
+
         this.add = this.main.querySelector('.tabadd');
-        this.ul = this.main.querySelector('.firstnav ul:first-child');
+        this.ul = this.main.querySelector('.fisrstnav ul:first-child');
+        this.fsection = this.main.querySelector('.tabscon');
+
         this.init();
     }
+
+
     init() {
+        this.updateNode();
 
         this.add.onclick = this.addTab;
         for (var i = 0; i < this.lists.length; i++) {
             this.lists[i].index = i;
             this.lists[i].onclick = this.toggleTab;
-
+            this.remove[i].onclick = this.removeTab;
+            this.spans[i].ondblclick = this.editTab;
+            this.sections[i].ondblclick = this.editTab;
         }
+
+
+    }
+    updateNode(){
+        this.lists = this.main.querySelectorAll('li');
+        this.sections = this.main.querySelectorAll('section');
+        this.remove = this.main.querySelectorAll('.icon-guanbi');
+        this.spans = this.main.querySelectorAll('.fisrstnav li span:first-child');
+
+
     }
 
     toggleTab (){
@@ -34,14 +50,46 @@ class Tab{
 
     addTab (){
         that.clearClass();
-        var li = '<li class="liactive"><span>新选项卡</span><span class="iconfont icon-guanbi"></span></li>'
+        var random = Math.random();
+        var li = '<li class="liactive"><span>New Tab</span><span class="iconfont icon-guanbi"></span></li>'
         that.ul.insertAdjacentHTML('beforeend', li);
+        var section = '<section class="conactive">测试'+random+'</section>'
+        that.fsection.insertAdjacentHTML('beforeend', section);
         that.init();
         console.log(1111);
+
     }
 
-    removeTab (){}
+    removeTab (event){
+        event.stopPropagation();
+        var index = this.parentNode.index;
+        console.log(index);
+        that.lists[index].remove();
+        that.sections[index].remove();
+        that.init();
+        if(document.querySelector('.liactive')) return;
+        index--;
+        that.lists[index] && that.lists[index].click();
 
-    editTab (){}
+    }
+
+    editTab (){
+        var str = this.innerHTML;
+        window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty()
+        this.innerHTML = '<input type="text"/>';
+        var input = this.children[0];
+        input.value = str;
+        input.select();
+        input.onblur = function (){
+            this.parentNode.innerHTML = this.value;
+        }
+        input.onkeyup = function (e){
+            if(e.key === 'Enter'){
+                this.onblur();
+            }
+        }
+
+
+    }
 }
 new Tab('#tab');
